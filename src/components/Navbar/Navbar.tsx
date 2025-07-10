e8ed02f0f72ba45d5f710b32bd16a79aa446795f
Daniel Wennström <daniel@wennstrom.io>
2025-07-10 13:45:32 +0200

    Basic layout

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="text-white bg-black px-5">
+      <div className="sm:flex space-x-6 items-center my-3">
+        <h5>Home</h5>
+        <div className="flex-grow text-right">
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='h-screen flex flex-col'>
+      <App />
+    </div>
   </StrictMode>,
 )
